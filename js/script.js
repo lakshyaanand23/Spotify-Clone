c5409dd09186fe50d437d37700a01621893f1b3d
Lakshya Anand <lakshyaanand2323@gmail.com>
2025-09-07 01:15:51 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -80,7 +80,7 @@ async function displayAlbums() {
         if (e.href.includes("\songs") &&!e.href.includes(".htaccess")) {
             let folder = e.href.split("/").slice(-2)[0];
             //Get the metadata of the folder
-            let a = await fetch(`/songs/${folder}/info.json`)
+            let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
             let response = await a.json();
             console.log(response);
             cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
